0c59b669180534c670155345b0ed4a9cbae1e0bc
PDB Corp <53020579+jaradat-pdb@users.noreply.github.com>
2019-08-03 01:46:34 -0400

    Refactor - added TEMPLATE to class variable name

--- src/main/java/corp/pdb/sio/demo/error/RegisteredUserDocNotFoundException.java
+++ src/main/java/corp/pdb/sio/demo/error/RegisteredUserDocNotFoundException.java
@@ -6,17 +6,17 @@ package corp.pdb.sio.demo.error;
  */
 public class RegisteredUserDocNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -7205354111515780651L;
-    private static final String EX_MSG = "Could not find a registered user in system via query by: {name: %s}";
+    private static final String EX_MSG_TEMPLATE = "Could not find a registered user in system via query by: {name: %s}";
 
     public RegisteredUserDocNotFoundException(String name) {
-        super(String.format(EX_MSG, name), new NullPointerException(String.format(EX_MSG, name)), false, false);
+        super(String.format(EX_MSG_TEMPLATE, name), new NullPointerException(String.format(EX_MSG, name)), false, false);
     }
 
     public RegisteredUserDocNotFoundException(String name, Exception e) {
-        super(String.format(EX_MSG, name), e, true, true);
+        super(String.format(EX_MSG_TEMPLATE, name), e, true, true);
     }
 
     public RegisteredUserDocNotFoundException(String name, Exception e, boolean enableSuppression, boolean writableStackTrace) {
-        super(String.format(EX_MSG, name), e, enableSuppression, writableStackTrace);
+        super(String.format(EX_MSG_TEMPLATE, name), e, enableSuppression, writableStackTrace);
     }
 }
